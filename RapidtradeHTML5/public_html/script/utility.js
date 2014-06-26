abe40e8b69ffc8afd816dfe1f53b888682b7ce37
miroslavgajic <miroslavgajic@abe960eb-43e5-46d0-8b3c-c5baf8c0559e>
2014-06-26 14:31:07 +0000

    - category descriptions in the pricelist

--- RapidtradeHTML5/public_html/script/utility.js
+++ RapidtradeHTML5/public_html/script/utility.js
@@ -69,47 +69,57 @@ var categories = (function(){
 	var instance;
 	
 	function catObj() {
+            
+            var that = this;
 		
-		this.init = function (){
-			this.acategories = [];
-		};
-		
-		this.addCategory = function (category){
-			try {			
-				if (this.acategories.indexOf(category) == -1) this.acategories.push(category);
-			} catch (err){
-				console.log(err.message);
-			}	
-		};
-		
-		this.showPopup = function (ultag, popuptag){
-			try {
-				if (this.acategories.length == 1) {
-					pricelistCategorySearch(this.acategories[0]);
-					return;
-				}
-				
-				var html = ' <li data-role="divider" data-theme="e">Choose category</li>';
-				for (var i=0;i < this.acategories.length;i++){
-					html += '<li><a href="#" onclick="categories.getInstance().fetchCategory(\'' + this.acategories[i] + '\',\'' + popuptag + '\' )">'  + this.acategories[i] + '</a></li>';
-				}
-				$(ultag).empty();
-				g_append(ultag, html);
-				$(ultag).listview('refresh');
-				//$(ultag).trigger('create');
-								
-				$( popuptag ).popup( 'open' );
-				
-			} catch (err){
-				console.log(err.message);
-			}	
-		};
-		
-		this.fetchCategory = function (category, popuptag){
-			$( popuptag ).popup('close');
-			pricelistCategorySearch(category);
-			
-		};
+            this.init = function (){
+                this.acategories = [];    
+                this.categoryDescriptions = [];
+            };
+
+            this.addCategory = function (category){
+                try {			
+                    if (this.acategories.indexOf(category) == -1) {
+                        this.acategories.push(category);
+
+                        var dao = new Dao();
+                        dao.index('ProductCategories2', category, 'index2', function(item) {
+                            that.categoryDescriptions.push(item.des)
+                        });
+                    }
+                } catch (err){
+                    console.log(err.message);
+                }	
+            };
+
+            this.showPopup = function (ultag, popuptag){
+                    try {
+                            if (this.acategories.length == 1) {
+                                pricelistCategorySearch(this.acategories[0]);
+                                return;
+                            }
+
+                            var html = ' <li data-role="divider" data-theme="e">Choose category</li>';
+                            for (var i=0;i < this.acategories.length;i++){
+                                html += '<li><a href="#" onclick="categories.getInstance().fetchCategory(\'' + this.acategories[i] + '\',\'' + popuptag + '\' )">'  + this.categoryDescriptions[i] + '</a></li>';
+                            }
+                            $(ultag).empty();
+                            g_append(ultag, html);
+                            $(ultag).listview('refresh');
+                            //$(ultag).trigger('create');
+
+                            $( popuptag ).popup( 'open' );
+
+                    } catch (err){
+                            console.log(err.message);
+                    }	
+            };
+
+            this.fetchCategory = function (category, popuptag){
+                    $( popuptag ).popup('close');
+                    pricelistCategorySearch(category);
+
+            };
 		
 	};
 	
