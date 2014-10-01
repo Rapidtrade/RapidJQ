7e6cd88059e3cf650f161b8e18251ed8ffa6ff49
miroslavgajic <miroslavgajic@abe960eb-43e5-46d0-8b3c-c5baf8c0559e>
2014-10-01 10:58:05 +0000

    - removed pading on catalogue images

--- RapidtradeHTML5/public_html/script/catalogue.js
+++ RapidtradeHTML5/public_html/script/catalogue.js
@@ -70,7 +70,7 @@ var catalogue = (function() {
             
             catalogueHTML += '<td style="vertical-align: bottom;padding:10px 15px;">';          
             
-            catalogueHTML += '<div style="width:300px;text-align:center;vertical-align:middle"><img src="' + productdetailGetImageUrl(item.ProductID, 160, false) + '" style="padding-bottom:20px"></div>' +
+            catalogueHTML += '<div style="width:300px;text-align:center;vertical-align:middle"><img src="' + productdetailGetImageUrl(item.ProductID, 160, false) + '"></div>' +
                     '<table><tr><td>Item</td><td>' + item.ProductID + '</td></tr>' +
                     '<tr><td>Descr</td><td>' + item.Description  + '</td></tr>' +
                     '<tr><td>Inn/Ctn Qty</td><td>' + (item.UserField02 || 'N/A')  + '</td></tr>' +
