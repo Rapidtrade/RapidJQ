01347766517f369e35d518cb23ac12a428515b56
adamfain <adamfain@abe960eb-43e5-46d0-8b3c-c5baf8c0559e>
2014-10-16 06:46:30 +0000

    globals.js modified to point SG's testrest

--- RapidtradeHTML5/public_html/script/globals.js
+++ RapidtradeHTML5/public_html/script/globals.js
@@ -4,14 +4,15 @@
 
 var g_logo = 'img/rapidtrade-logo-small.png';
 
-//var g_url = "http://www.dedicatedsolutions.co.za:8082/";
+var g_url = "http://www.dedicatedsolutions.co.za:8082/";
 //var g_restUrl = g_url + "rest2/";
-//var g_vanSales = true;
+var g_restUrl = g_url + "testrest/";
+var g_vanSales = true;
 var g_restPHPUrl = "www.super-trade.co.za:8083/rest/index.php";
 
-var g_url = "https://app.rapidtrade.biz/";
-var g_restUrl = g_url + "rest/";
-var g_vanSales = false;
+//var g_url = "https://app.rapidtrade.biz/";
+//var g_restUrl = g_url + "rest/";
+//var g_vanSales = false;
 //var g_restPHPUrl = "http://107.21.55.154/rest/index.php/";
 
 var g_indexedDB;
